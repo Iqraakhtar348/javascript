64f26b3c4ec58c6c46221ec8fee1f92fe9b24e60
Iqraakhtar348 <iqraakhtar348@gmail.com>
2024-11-10 23:56:52 +0500

    my javascript

---
+++ javascript/iqra.js
@@ -0,0 +1,6 @@
+var iqra="it my first day in javascript"
+console.log(iqra)
+let iqra2="it my first day in javascript and plz focus"
+console.log(iqra2)
+const iqra3=876
+console.log(iqra3)
---
+++ js/app.js
@@ -0,0 +1,28 @@
+// console.error("Error")
+// console.warn("Warning")
+// console.log("Welcome iqra")
+
+// types of variables in javascrilpt
+
+/*
+ 1 var = yeah purana tariqa hai variable declare karne ka is mein function scope hota aur yeah redeclar bhi   ho sakta hain or change bhe ho skata hai
+ 2 let = yeah block-scope variable hain jo redeclare nahi ho sakta, lakin update ho sakta hai
+ 3 const = yeah constant variable hai jo ek baar assign hone ke baad change nai ho sakta
+*/
+
+//  not recemond
+// var iqra = "my name is iqra";
+// iqra = "my name is changed";
+// var iqra = "mein aik new variable hu"
+
+// let iqra2= "am software engineer";
+// iqra2= "Am frontend developer";
+// let iqra2 = "mein error duga"
+
+const iqra3 = "am developer"
+// iqra3 = "update const variable"
+// const iqra3= "something"
+
+// console.log(iqra);
+// console.log(iqra2);
+console.log(iqra3);

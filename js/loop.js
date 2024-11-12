65a8ec57fe690c18379847961c352c41986c20bc
Iqraakhtar348 <iqraakhtar348@gmail.com>
2024-11-13 01:37:24 +0500

    array method done

--- js/loop.js
+++ js/loop.js
@@ -1,13 +1,9 @@
-
 const isTrue= true;
 
 // if(isTrue){
-
 //     for (let number= 1; number <= 10; number++) {
-
-//         console.log(number)
-        
-//     }
+//         console.log(number)        
+// }
 // }
 
 // let number = 2;
@@ -17,8 +13,26 @@ const isTrue= true;
 //     console.log(number)
 // }
 
-                //  0            1         2        3      4       5       6
-const languages= ["javascript", "python", "Elixer", "Go", "Rust", "Node", "C++"]
+// for(let number= 0 ; number < 10; number ++){
+// console.log(number)
+// }
+// for(let number=0; number<100; number ++){
+//     console.log(number)
+// }
+// let number=17;
+// while (number< 14){
+//     number++
+//     console.log(number)
+// }
+// if (a=4) {
+//     console.log("a is right")
+// }else if(a=3){
+//     console.log("a is not right")
+// }
+
+   
+//  0   1    2    3    4    5    6
+// const languages= ["javascript", "python", "Elixer", "Go", "Rust", "Node", "C++"]
 
 // languages.push("iqra") Array ke end mein elemetn add karta
 // languages.pop() Array ke end se element remove karta hai
@@ -31,11 +45,43 @@ const languages= ["javascript", "python", "Elixer", "Go", "Rust", "Node", "C++"]
 // console.log(languages)
 
 // for (const output of languages) {
-    
-//     console.log(output);
-    
+// console.log(output); 
 // }
 
 // for (const output in languages) {
 //     console.log(output);
 // }
+// const language=["course","java","python"]
+// language.reverse()
+// language.fill("python")
+// language.push("iqra")
+// language.pop()
+// language.splice(1,0,"software engineer")
+// language.sort()
+// language.join()
+// console.log(language)
+
+//   function address(a,b) {
+//     if(a=="lahore"){
+//         console.log("lahore is the city of pakistan")
+//     }else if(b=="sprius"){
+//         console.log("that is the city of europe")
+//     }
+//   }
+
+//   address("lahores", "sprius")
+
+
+const languages1 =["Elixer", "Go", "Rust"]
+// const languages= ["javascript", "python"]
+// const c= languages1.concat(languages)
+// const output = languages.indexOf("Go")
+// let index = languages1.indexOf("Rust")
+
+// if (languages1.includes("Rust")) {
+    
+//     console.log("Yes")
+// }
+
+const output = languages1.toString()
+console.log(output)
